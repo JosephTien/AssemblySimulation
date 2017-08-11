50ca2638f2796244fc4e6b5475b50daa61ec3ab3
JosephTien <joseph811223@gmail.com>
2017-08-11 16:52:20 +0800

    ...

---
+++ csgcommandlinetool/@.bat
@@ -0,0 +1,1 @@
+CSGCommandLineTool.exe script2.txt
---
+++ csgcommandlinetool/_.bat
@@ -0,0 +1,8 @@
+del ..\inputSet\202\inputObj\*.obj
+mkdir ..\inputSet\202\inputObj\
+copy pool\finesphere_0.obj ..\inputSet\202\inputObj\
+move ..\inputSet\202\inputObj\finesphere_0.obj ..\inputSet\202\inputObj\output_0.obj
+copy pool\sphere_1.obj ..\inputSet\202\inputObj\
+move ..\inputSet\202\inputObj\sphere_1.obj ..\inputSet\202\inputObj\output_1.obj
+copy pool\comp_7.obj ..\inputSet\202\inputObj\
+move ..\inputSet\202\inputObj\comp_7.obj ..\inputSet\202\inputObj\output_2.obj
